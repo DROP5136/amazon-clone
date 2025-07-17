bd589436be5eba9a7ff1e239852972c3aa0d0f4c
DROP5136 <batralakshay16@gmail.com>
2025-07-17 18:52:51 +0530

    added-product.js-file

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,5 @@
-const product=[{
-    image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating:{
-        star:4.5,
-        count:110
-    },
-    price:150
-},
-{
-    image:"images/products/intermediate-composite-basketball.jpg",
-    name:'Intermediate Size Basketball',
-    rating:{
-        star:4,
-        count:534
-    },
-    price:999
-},
-{
-    image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name:'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        star:4.5,
-        count:15
-    },
-    price:300
-}
-];
 let producthtml=""
-product.forEach(product =>{
+products.forEach(product =>{
 producthtml+=`
 <div class="product-container">
           <div class="product-image-container">
@@ -41,7 +13,7 @@ producthtml+=`
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.star * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
